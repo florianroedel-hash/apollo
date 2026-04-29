c74c549d827a4ce18c34ed0e99986ce67214e2ef
florianroedel-hash <florianroedel@gmail.com>
2026-04-29 22:46:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbyl6Uj0xd6qmyb69sLF63RvJN7SRsppgwm2FwW_sVvtJ8bJTAAc5msS_oq_scI7u8M/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycby3AgRD49QItpR6M3oKG0id58QCZN0a7zQbrm91Z1ZmjwvhBwJzLNI3xBuANUzsWaiVfA/exec';
 const pile = document.getElementById('project-pile');
 const logoContainer = document.getElementById('logo-container');
 let archiveData = [];
